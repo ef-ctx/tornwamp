8d4cb68758878cc2cffe6f59cf58ec84f6520b47
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2014-11-14 11:31:10 +0000

    Add WAMP ABORT message

--- tests/test_message.py
+++ tests/test_message.py
@@ -19,3 +19,19 @@ class MessageTestCase(unittest.TestCase):
         self.assertEqual(hello_message.realm, "world")
         self.assertEqual(hello_message.details, {"roles": {}})
         self.assertEqual(hello_message.json, '[1, "world", {"roles": {}}]')
+
+    def test_abort_message(self):
+        abort_message = wamp.AbortMessage(reason="user.unauthorized")
+        abort_message.error("Invalid cookie")
+        self.assertEqual(abort_message.code, wamp.ABORT)
+        self.assertEqual(abort_message.details, {"message": "Invalid cookie"})
+        self.assertEqual(abort_message.reason, "user.unauthorized")
+        expected = '[3, {"message": "Invalid cookie"}, "user.unauthorized"]'
+        self.assertEqual(abort_message.json, expected)
+
+    def test_abort_message_without_reason(self):
+        with self.assertRaises(AssertionError) as error:
+            abort_message = wamp.AbortMessage()
+        computed = str(error.exception)
+        expected = "AbortMessage must have a reason"
+        self.assertEqual(computed, expected)
--- tornwamp/messages.py
+++ tornwamp/messages.py
@@ -1,10 +1,13 @@
 """
 WAMP messages definitions and serializers.
+
+Compatible with WAMP Document Revision: RC3, 2014/08/25, available at:
+https://github.com/tavendo/WAMP/blob/master/spec/basic.md
 """
 
 import json
 
-# WAMP codes
+
 HELLO = 1
 WELCOME = 2
 ABORT = 3
@@ -36,6 +39,8 @@ class Message(object):
     """
     Represent any WAMP message.
     """
+    details = {}
+
     def __init__(self, code, *args):
         self.code = code
         self.value = [code] + list(args)
@@ -47,21 +52,40 @@ class Message(object):
         """
         return json.dumps(self.value)
 
+    def error(self, text):
+        """
+        Add error description. This is mainly useful for WAMP messages which
+        have a details dictionary.
+        """
+        self.details["message"] = text
+
 
 class HelloMessage(Message):
     """
     Sent by a Client to initiate opening of a WAMP session:
     [HELLO, Realm|uri, Details|dict]
+
+    https://github.com/tavendo/WAMP/blob/master/spec/basic.md#hello
     """
 
     def __init__(self, code=HELLO, realm="", details=None):
         self.code = code
         self.realm = realm
         self.details = details if details else {}
+        self.value = [self.code, self.realm, self.details]
 
-    @property
-    def value(self):
-        """
-        Define the structure to be used to generate a JSON WAMP-compatible.
-        """
-        return [self.code, self.realm, self.details]
+
+class AbortMessage(Message):
+    """
+    Both the Router and the Client may abort the opening of a WAMP session
+    [ABORT, Details|dict, Reason|uri]
+
+    https://github.com/tavendo/WAMP/blob/master/spec/basic.md#abort
+    """
+
+    def __init__(self, code=ABORT, details=None, reason=None):
+        assert not reason is None, "AbortMessage must have a reason"
+        self.code = code
+        self.details = details if details else {}
+        self.reason = reason
+        self.value = [self.code, self.details, self.reason]
